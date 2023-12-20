4ed50fa0b0f58fe123b8b8368304c8f70447b9de
SourceSafe <whitesteel@btinternet.com>
2023-12-20 20:54:18 +0000

    Checkpoint

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div>
-      <h1>Student Accommodation To Rent in the UK</h1>
+      <h1>Student Accommodation For Rent</h1>
       <ul>
         <li> <Link to="/aberdeen">Aberdeen</Link></li>
         <li><Link to="/aberystwyth">Aberystwyth</Link></li>
