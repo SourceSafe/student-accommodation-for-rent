fff352c06d266478e58a748b2fceb158c89d2066
SourceSafe <whitesteel@btinternet.com>
2024-01-20 20:50:03 +0000

    Checkpoint

--- src/portal/Listing/Listing.jsx
+++ src/portal/Listing/Listing.jsx
@@ -3,23 +3,45 @@ import React from 'react'
 import "./listing.css"  
 export const  Listing = ({listing}) =>
 {  
+
+
+
+  const info = listing.addedReduced;
+  
+  
   return (
 
 
 
     <div className="listing">
       <div className="listing-lhs">
-      
+        
             <img className="image1" src={listing.image1}/>                                 
-            <div className="price-container">                        
-              <div className ="listing-pcm" >
-              £{listing.price1} per month
+
+            <div className = "belowImageInfoContainer">                        
+            
+
+              <div>                            
+                <div className ="listing-pcm" >
+                  £{listing.price1} per month
+                </div>
+                <div className ="listing-pw" >
+                  £{listing.price2} per week
+                </div>                  
               </div>
-              <div className ="listing-pw" >
-              £{listing.price2} per week
-              </div>          
-            </div>            
-      
+
+              
+
+                { info &&<div className = "addedRecuded">
+                  <div>{ listing.addedReduced}</div>                  
+                </div> }                  
+
+                      
+
+
+            </div>        
+
+
       </div>
       <div className="listing-rhs">
 
@@ -31,7 +53,6 @@ export const  Listing = ({listing}) =>
 
         <div className= "listing-text"> 
         {listing.description}
-
         </div>
         
 
