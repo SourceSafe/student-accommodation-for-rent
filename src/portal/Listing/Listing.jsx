34778f68a07e45503bedbfb2c113768d41a11079
SourceSafe <whitesteel@btinternet.com>
2024-01-21 13:57:21 +0000

    CHeckpoint

--- src/portal/Listing/Listing.jsx
+++ src/portal/Listing/Listing.jsx
@@ -1,12 +1,40 @@
 
 import React from 'react'
+import { useState, useEffect } from 'react';
 import "./listing.css"  
+
+
 export const  Listing = ({listing}) =>
 {  
 
 
+  const [windowSize, setWindowSize] = useState([
+    window.innerWidth,
+    window.innerHeight,
+  ]);
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize([window.innerWidth, window.innerHeight]);
+    };
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, []);
+
+
 
   const info = listing.addedReduced;
+
+  const imageWidth = windowSize[0] < 600 ?  windowSize[0]-50 : 350;
+  const imageHeight = windowSize[0] < 600 ?  277 : 'auto';
+
+  const isDesktop = windowSize[0] > 600;
+
+  
   
   
   return (
@@ -14,9 +42,10 @@ export const  Listing = ({listing}) =>
 
 
     <div className="listing">
+ 
       <div className="listing-lhs">
         
-            <img className="image1" src={listing.image1}/>                                 
+            <img className="image1" style={{width :imageWidth, height:imageHeight}} src={listing.image1}/>                                 
 
             <div className = "belowImageInfoContainer">                        
             
@@ -27,36 +56,34 @@ export const  Listing = ({listing}) =>
                 </div>
                 <div className ="listing-pw" >
                   £{listing.price2} per week
+
                 </div>                  
+                <div>
+          <span>Width: {windowSize[0]}</span>
+          <span>Height: {windowSize[1]}</span>
+        </div>      
               </div>
 
+          
               
-
                 { info &&<div className = "addedRecuded">
                   <div>{ listing.addedReduced}</div>                  
                 </div> }                  
-
-                      
-
-
+                    
             </div>        
-
-
       </div>
-      <div className="listing-rhs">
 
 
-          <div className= "listing-address"> {listing.add1}</div>                  
-
-        
+      {isDesktop && <div className="listing-rhs">
+        <div className= "listing-address"> {listing.add1}</div>                          
         <div className= "listing-subheader"> {listing.add2}</div>
-
         <div className= "listing-text"> 
-        {listing.description}
+        {decodeURIComponent(listing.description)}
         </div>
-        
+         
+      </div> }
 
-      </div>           
+
     </div>
     
     
