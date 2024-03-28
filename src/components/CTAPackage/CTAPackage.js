b65a134fbea238eae3d29b4e0bad3ef93d7c6a08
SourceSafe <whitesteel@btinternet.com>
2024-03-28 07:01:28 +0000

    Checkpoint

--- src/components/CTAPackage/CTAPackage.js
+++ src/components/CTAPackage/CTAPackage.js
@@ -1,7 +1,6 @@
 import  './CTAPackage.css'
 import { BsFillHouseCheckFill } from "react-icons/bs";
 
-import  {Link} from 'react-router-dom';
 export const CTAPackage = (props) =>
 {
 const {description} = props;
@@ -9,21 +8,12 @@ const tag1 = "All Bills Included"
 const tag2 = "Get an Instant Quote Now"
 
 
-const route = () =>
-{
-return(      
-    <div>
-    <Link to={{ pathname: "https://herewecode.io/" }} target="_blank">Click to open HereWeCode (new tab)</Link>
-  </div>)
 
-
-}
 
 return(  
     
-    
-    <a href="https://join.splitthebills.co.uk/" target="_blank"> 
-        <button onClick={route} className='ctaButton'>            
+    <a style = {{ textDecoration: 'none', color: 'white' }} href="https://join.splitthebills.co.uk/" target="_blank"> 
+        <button className='ctaButton'>            
         <div className="ctaLayout">
             <div className="ctaTextLayout">
                 <div className="ctaText">{tag1}</div>                    
