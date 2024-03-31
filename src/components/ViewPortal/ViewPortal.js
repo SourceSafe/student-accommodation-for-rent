1fc32a131fbcc379f2d81a26e448c99352ca5fd3
SourceSafe <whitesteel@btinternet.com>
2024-03-31 11:19:05 +0100

    Link Main Portal Button

--- src/components/ViewPortal/ViewPortal.js
+++ src/components/ViewPortal/ViewPortal.js
@@ -1,11 +1,41 @@
 import  './ViewPortal.css'
+import {useState, useEffect}  from 'react'
+import { atom, useAtomState } from '@zedux/react';
 import { BsFillHouseFill } from "react-icons/bs";
+import { filtersAtom  } from '../../portal/appState/appState';
+import {useNavigate} from 'react-router-dom'
 
 export const ViewPortal = (props) =>
 {
-    const {description, portalLink} = props;   
+    const navigate = useNavigate();
+    const [buttonClicked, setButtonClicked] = useState(false);
+    const [filters, setFilters] = useAtomState(filtersAtom);              
+    const {description, queryParams} = props;   
+    
+
+    useEffect(()=> 
+    {
+        if(buttonClicked && filters) {
+            navigate("/")
+        }        
+    }    
+    ,[buttonClicked, filters]
+    )
+    const routeToPortal = () => {                  
+        const queryParameters = new URLSearchParams(queryParams)
+        const town= queryParameters.get("townLocationId");
+        const location = queryParameters.get("areaLocationId");
+        const beds= queryParameters.get("beds");
+        const homeType = queryParameters.get("type");                         
+        const newFilter = { town, location, beds, homeType};
+        setFilters(newFilter)          
+        setButtonClicked( (state) => Number(state) + 1 )
+        console.log(newFilter);
+      }
+      
+
     return(    
-        <button  className='viewPortalButton'>            
+        <button  className='viewPortalButton' onClick={() => routeToPortal()}>            
             <div className="viewPortalLayout">
                 <div className="viewPortalTextLayout">
                     <div className="viewPortalText">{description}</div>                                        
